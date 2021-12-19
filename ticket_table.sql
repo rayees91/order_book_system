e63bfd3caf69455dffaaea0546003bd4a7f79cc7
rayees91 <rayees9010@gmail.com>
2021-12-19 12:47:28 +0530

    temp

--- ticket_table.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE `ticker_tbl` (
-  `ticker_id` varchar(25) NOT NULL,
-  `ticker_name` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`ticker_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
