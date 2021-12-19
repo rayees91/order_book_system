deb304b4f7ead2e601c609547ff320d13444ff32
rayees91 <78843334+rayees91@users.noreply.github.com>
2021-12-19 12:06:42 +0530

    Delete user_table.sql

--- user_table.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `user_tbl` (
-  `user_id` int NOT NULL,
-  `user_name` varchar(45) DEFAULT NULL,
-  `access_level` varchar(20) DEFAULT NULL,
-  PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
